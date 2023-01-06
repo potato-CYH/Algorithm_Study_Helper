77d9b6621ae38f20371f42935563e34f352d0c65
차영후 <mydream11123@naver.com>
2023-01-06 14:49:18 +0900

    6th fix

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import Main from './main/Main'
 import MyPage from './MyPage/MyPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+  
       <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/main" element={<Main name={'main'}/>}/>
+          <Route path="/Algorithm_Study_Helper/main" element={<Main name={'main'}/>}/>
           <Route path="/timer" element={<Main name={'timer'}/>}/>
           <Route path="/suggest" element={<Main name={'suggest'}/>}/>
           <Route path="/common" element={<Main name={'common'}/>}/>
           <Route path="/mypage" element={<MyPage />}/>
       </Routes>
-    </BrowserRouter>
   );
 }
 
